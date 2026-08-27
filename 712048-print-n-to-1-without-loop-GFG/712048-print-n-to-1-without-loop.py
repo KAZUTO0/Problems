8b64650afd0003930939f93d95b31569d30a9985
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-08-27 16:03:19 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 712048-print-n-to-1-without-loop-GFG/712048-print-n-to-1-without-loop.py
@@ -0,0 +1,12 @@
+class Solution:
+    def printNos(self, n):
+        def x(n):
+            if n==0:
+                return "False"
+            print(n, end=" ")
+            x(n-1)
+        return x(n)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
