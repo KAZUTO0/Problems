a9b4477d07c91a49409c894c08d42eb744b995e6
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-08-27 15:53:08 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.04%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n % 2 == 0:
+            n //= 2
+
+        return n == 1
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
