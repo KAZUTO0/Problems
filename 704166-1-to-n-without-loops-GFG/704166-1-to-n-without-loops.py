1157ff653cb960c5c4a67cd12dd47b3831bb9c09
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-08-27 15:29:57 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704166-1-to-n-without-loops-GFG/704166-1-to-n-without-loops.py
@@ -0,0 +1,8 @@
+class Solution:
+    def printTillN(self, n):
+    	for i in range(1, n+1):
+    	    print(i, end=" ")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
