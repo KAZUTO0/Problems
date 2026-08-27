10763272da182666ce9cdb41f87d228c5b3604cf
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-08-27 15:38:08 +0530

    Added solution - Time: O(log n), Space: O(log n)

---
+++ 703905-sum-of-digits-GFG/703905-sum-of-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfDigits(self, n):
+        x=0
+        for i in str(n):
+            x+=int(i)
+        return x
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
