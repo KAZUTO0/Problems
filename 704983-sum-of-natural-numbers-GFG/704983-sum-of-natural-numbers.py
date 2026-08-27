0c2432b56d22ef8b4b2a07bc9de96dffc4f7dd23
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-08-27 15:35:40 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704983-sum-of-natural-numbers-GFG/704983-sum-of-natural-numbers.py
@@ -0,0 +1,12 @@
+n = int(input())
+x=0
+if n==0:
+    print(0)
+else:
+    for i in range(1, n+1):
+        x+=i
+    print(x)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
