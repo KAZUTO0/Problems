7a74db9fe0ef0bccbcc8db2ae7df03d1c63a62fa
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-09-16 16:04:53 +0530

    Time: 0 ms (100%), Space: 19.2 MB (55.79%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n<=1:
+            return n
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        dp[2]=1
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+        return dp[n]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
