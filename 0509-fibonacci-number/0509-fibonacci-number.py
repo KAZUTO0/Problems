b342d38e15043d7b65b19c356ed58025ee04eb6a
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-09-16 15:59:54 +0530

    Time: 45 ms (70.29%), Space: 19.1 MB (88.96%) - LeetHub - Time: O(n), Space: O(n)

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0:
-            return 0
-        elif n==1:
-            return 1
-        return self.fib(n-1)+self.fib(n-2)
-        
+        if n<=1:
+            return n
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2, n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
