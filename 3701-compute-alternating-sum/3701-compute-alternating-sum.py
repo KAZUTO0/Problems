196af9c93713eb54f908d251b302815cbad86e6a
Sameer Kumar Karn <92509050+KAZUTO0@users.noreply.github.com>
2026-09-18 15:47:10 +0530

    Time: 0 ms (100%), Space: 19.3 MB (14.52%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def alternatingSum(self, nums: List[int]) -> int:
+        return sum(nums[::2])-sum(nums[1::2])
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
